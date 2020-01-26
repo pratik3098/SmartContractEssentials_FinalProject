eb24e9a22982bb3c57f92cb2ddd49e7b87ba94e9
pratik3098 <pratik3098@gmail.com>
2020-01-25 20:28:48 -0500

    ERC721 functionality added

--- ERC721Token.sol
+++ ERC721Token.sol
@@ -5,28 +5,64 @@ import "./ERC721Interface.sol";
 contract ERC721Token is ERC721 {
     address public owner;
     
-   
+    
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
     event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
     event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
-    mapping(address=> uint256) balances;
-    mapping(address=>uint)  tokens;
+    event UserCalldata(bytes);
+    mapping(address=> uint256) tokens;
+    mapping(uint256 => address)  token_owner;
+    mapping(address=>mapping(address=>uint256)) approved;
     
     function balanceOf(address _owner) external view returns (uint256){
-        require(balances[_owner]!= 0,"Error: Owner doesn't exist");
-        return balances[_owner];
+        require(tokens[_owner]!= 0,"Error: Owner doesn't exist");
+        return tokens[_owner];
     }
     
     function ownerOf(uint256 _tokenId) external view returns (address){
+        return(token_owner[_tokenId]);
+    }
+   
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata data) external payable onlySender(_from) {
+        this.safeTransferFrom(_from,_to,_tokenId);
+        emit UserCalldata(data);
+    }
+    
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable onlySender(_from) {
+        require(msg.sender==_from,"Error: Invalid invoker");
+        require(_tokenId!=0,"Error: Invalid token Id");
+        require(tokens[_from]==_tokenId,"Error: Invalid Owner");
+        require(approved[_from][_to]==_tokenId,"Error: Transfer not approved");
+        tokens[_from]=0;
+        token_owner[_tokenId]=_to;
+        tokens[_to]=0;
+        emit Transfer(_from, _to, _tokenId);
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable onlyOwner{
+        this.safeTransferFrom(_from,_to,_tokenId);
+    }
+    function approve(address _approved, uint256 _tokenId) external payable onlySender(_approved){
+        require(tokens[msg.sender]!=0,"Error: TokenId does not exist");
+        require(token_owner[_tokenId]==msg.sender,"Error: Not a token owner");
+        approved[msg.sender][_approved]= _tokenId;
+        emit Approval(msg.sender, _approved, _tokenId);
+    }
+    
+    function setApprovalForAll(address _operator, bool _approved) external onlyOwner{
         
     }
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata data) external payable;
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
-    function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
-    function approve(address _approved, uint256 _tokenId) external payable;
-    function setApprovalForAll(address _operator, bool _approved) external;
-    function getApproved(uint256 _tokenId) external view returns (address);
+    function getApproved(uint256 _tokenId) external view returns (address){
+        
+    }
     function isApprovedForAll(address _owner, address _operator) external view returns (bool);
     
-    
+    modifier onlyOwner{
+        require(msg.sender==owner, "Error: Only owner authorised");
+        _;
+    }
+    modifier onlySender(address _from){
+        require(msg.sender==_from || msg.sender == owner,"Error: Invalid invoker");
+        _;
+    }
 }
